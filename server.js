66d855f5883277048a9dc268e3405c93ba68faa0
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-28 17:49:52 -0500

    Installed nodemon and created a server that listens.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3002;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Navigate to /notes or /index'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/index', (req, res) =>
+    res.sendFile(path.join(__dirname, 'public/index.html'))
+);
+
+app.listen(PORT, () =>
+    console.log(`App Listening at http://localhost:${PORT}`)
+);
