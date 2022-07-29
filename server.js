933f46b6f281cc7236f9fa66328c58da65e1eaab
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-28 20:16:37 -0500

    server.js update.

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ const PORT = 3002;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /notes or /index'));
-
+app.get('/', (req, res) => res.send('Navigate to /notes or /index')
+);
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
-
 app.get('/index', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
