f50262afb68609afae4bf90fcfb3a58d99d38f05
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-29 11:27:28 -0500

    Created paths to home/notes page. Get the data from db.json. Post title and text then update notes.

---
+++ routes/api.js
@@ -0,0 +1,61 @@
+const api = require('express').Router();
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+api.get("/notes", (req, res) => {
+    fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+         res.json(JSON.parse(data)) ;
+        }
+    })
+});
+
+api.post('/notes', (req, res) => {
+    const { text, title } = req.body;
+    if ( text && title) {
+        // Variable for the object we will save
+        const newNote = {
+          text,
+          title,
+          id: uuid(),
+        };
+    
+        // Obtain existing notes
+        fs.readFile('./Develop/db/db.json', 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+          } else {
+            // Convert string into JSON object
+            const parsedNotes = JSON.parse(data);
+    
+            // Add a new note
+            parsedNotes.push(newNote);
+    
+            // Write updated notes into the file
+            fs.writeFile(
+              './Develop/db/db.json',
+              JSON.stringify(parsedNotes, null, 4),
+              (writeErr) =>
+                writeErr
+                  ? console.error(writeErr)
+                  : console.info('Successfully updated reviews!')
+            );
+          }
+        });
+    
+        const response = {
+          status: 'success',
+          body: newNote,
+        };
+    
+        console.log(response);
+        res.status(201).json(response);
+      } else {
+        res.status(500).json('Error in posting review');
+      }
+});
+
+module.exports = api;
--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+const api = require('./routes/api');
 
 const app = express();
-const PORT = 3002;
+// Add a port
+const PORT = 3001;
 
-app.use(express.static('Develop/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("Develop/public"));
+app.use('/api', api);
 
-// Create routes that will serve up the 'index.html' home page
-app.get('/', (req, res) => res.send('Send to /index')
-);
 // Create routes that will serve up the 'notes.html' page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "Develop/public/notes.html"))
+);
+// Create routes that will serve up the 'index.html' home page
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "Develop/public/index.html"))
 );
 app.listen(PORT, () =>
-    console.log(`App Listening at http://localhost:${PORT}`)
+  console.log(`App Listening at http://localhost:${PORT}`)
 );
